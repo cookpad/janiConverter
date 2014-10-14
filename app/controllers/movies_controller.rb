154850f01eb39f74942ac9d5987c57982d2d8225
Ohno Shin'ichi <ohno@cookpad.com>
2014-10-14 18:37:47 +0900

    Movie create/show: very basics

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,13 @@
 class MoviesController < ApplicationController
+  respond_to :json, :html
+
+  def create
+    Movie.create()
+    redirect_to "show"
+  end
+
+  def show
+    @movie = Movie.where(id: params[:id]).first()
+    respond_with @movie
+  end
 end
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe MoviesController, :type => :controller do
+describe MoviesController, type: :controller do
+  describe "create" do
+    subject { post :create }
 
+    it "creates a movie" do
+      expect{ subject }.to change{ Movie.count(1) }.by(1)
+    end
+  end
+
+  describe "show" do
+    let(:movie)  { create(:movie) }
+    subject { get :show, id: movie.id, format: :json  }
+
+    it "shows the movie" do
+      subject
+      expect(assigns(:movie).id).to eq(movie.id)
+    end
+  end
 end
