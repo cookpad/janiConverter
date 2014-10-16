8615e74dab66fbd6c29c32679ca48f315a58ab37
Ohno Shin'ichi <ohno@cookpad.com>
2014-10-16 15:26:19 +0900

    Refactor: Move setting logic to StorageSettings class

--- app/settings/storage_settings.rb
+++ app/settings/storage_settings.rb
@@ -1,5 +1,20 @@
-
 class StorageSettings < Settingslogic
   source "#{Rails.root}/config/strages.yml"
   namespace Rails.env
+
+  def self.set_to_uploader(uploader, storage)
+    unless Rails.env.test?
+      publish_settings = StorageSettings.send(storage.to_sym)
+      uploader.fog_credentials = {
+        :provider               => publish_settings.provider,
+        :aws_access_key_id      => publish_settings.access_key_id,
+        :aws_secret_access_key  => publish_settings.secret_access_key,
+        :region                 => publish_settings.region,
+        :path_style             => publish_settings.path_style,
+      }
+      uploader.fog_directory  = publish_settings.directory
+      uploader.fog_public     = publish_settings.public
+      uploader.fog_attributes = {'Cache-Control'=>"max-age=#{publish_settings.max_age}"}
+    end
+  end
 end
--- app/uploaders/strip_uploader.rb
+++ app/uploaders/strip_uploader.rb
@@ -4,20 +4,7 @@ class StripUploader < CarrierWave::Uploader::Base
   # see https://github.com/carrierwaveuploader/carrierwave/wiki/How-to:-Define-different-storage-configuration-for-each-Uploader.
   def initialize(*)
     super
-
-    unless Rails.env.test?
-      publish_settings = StorageSettings.publish
-      self.fog_credentials = {
-        :provider               => publish_settings.provider,
-        :aws_access_key_id      => publish_settings.access_key_id,
-        :aws_secret_access_key  => publish_settings.secret_access_key,
-        :region                 => publish_settings.region,
-        :path_style             => publish_settings.path_style,
-      }
-      self.fog_directory  = publish_settings.directory
-      self.fog_public     = publish_settings.public
-      self.fog_attributes = {'Cache-Control'=>"max-age=#{publish_settings.max_age}"}
-    end
+    StorageSettings.set_to_uploader(self, :publish)
   end
 
   def store_dir
