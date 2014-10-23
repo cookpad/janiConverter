e8e83a55b3363c72654dc8bc179a4cbf84ee518a
Ohno Shin'ichi <ohno@cookpad.com>
2014-10-23 23:47:22 +0900

    Create event trackings

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,7 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.create(movie_params(params).merge(uuid: SecureRandom.uuid))
+    create_tracking_events_if_params_available(@movie, params)
     Converter.perform_async(@movie.uuid)
     redirect_to @movie
   end
@@ -19,6 +20,18 @@ class MoviesController < ApplicationController
   private
 
   def movie_params(params)
-    params.require(:movie).permit(:fps, :frame_height, :frame_width, :movie, :movie_cache)
+    params.require(:movie).permit(:fps, :frame_height, :frame_width, :movie, :movie_cache, :tracking_events)
+  end
+
+  def create_tracking_events_if_params_available(movie, params)
+    tracking_events_params = params[Movie.class_name.underscore][TrackingEvent.class_name.pluralize.underscore]
+    return unless tracking_events_params
+    tracking_events_params.each do |event_name, parameters|
+      @movie.tracking_events.build.tap do |tracking_event|
+        tracking_event.label = parameters["label"]
+        tracking_event.url = parameters["url"]
+        tracking_event.save
+      end
+    end
   end
 end
