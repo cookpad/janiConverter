e4da473e7a9c2656e1ccf9f671f36f9c6b5592bc
Ohno Shin'ichi <ohno@cookpad.com>
2014-10-27 16:43:56 +0900

    Serve images through CDN / change uploader class name:   app/uploaders/strip_uploader.rb -> app/uploaders/image_publisher.rb

--- app/uploaders/image_publisher.rb
+++ app/uploaders/image_publisher.rb
@@ -1,4 +1,4 @@
-class StripUploader < CarrierWave::Uploader::Base
+class ImagePublisher < CarrierWave::Uploader::Base
   include ::CarrierWave::Backgrounder::Delay
 
   # see https://github.com/carrierwaveuploader/carrierwave/wiki/How-to:-Define-different-storage-configuration-for-each-Uploader.
@@ -10,4 +10,16 @@ class StripUploader < CarrierWave::Uploader::Base
   def store_dir
     "#{Rails.env}/strips/#{model.movie.uuid}"
   end
+
+  module CDNURLFix
+    def url
+      if asset_host && fog_credentials.present? && fog_credentials[:path_style]
+        fog_directory.match(/\/(.*)/)
+        [asset_host, $1, path].join("/")
+      else
+        super
+      end
+    end
+  end
+  include CDNURLFix
 end
