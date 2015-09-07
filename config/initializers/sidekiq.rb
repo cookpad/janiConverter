d734f34d2580a2066df3884ee484ceba2cdf113f
Ohno Shin'ichi <ohno@cookpad.com>
2015-09-07 16:20:21 +0900

    Sidekiq logging

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,11 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: RedisSettings.client.url, namespace: RedisSettings.client.namespace }
 end
+
+Sidekiq::Logging.logger = if Rails.env.test?
+    nil
+  elsif ENV["LOG_DIR_PATH"].present?
+    Logger.new("#{ENV["LOG_DIR_PATH"]}/#{Rails.env}.log")
+  else
+    Logger.new(STDOUT)
+  end
